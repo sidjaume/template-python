26f5c6a963e78c1dbb299b57f5a0cbc9815083d5
sidjaume <119365997+sidjaume@users.noreply.github.com>
2023-09-07 16:17:25 +0200

    Update app.py
    Signed-off-by: sidjaume <119365997+sidjaume@users.noreply.github.com>

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 from sqlalchemy import create_engine
 
 # Crea una conexión a la base de datos
-engine = create_engine('postgresql://fl0user:9aIMLsfioV0r@ep-odd-cell-06464893.eu-central-1.aws.neon.tech:5432/postgresql?sslmode=require')
+engine = create_engine('postgres://fl0user:9aIMLsfioV0r@ep-odd-cell-06464893.eu-central-1.aws.neon.tech:5432/postgres?sslmode=require')
 
 def error():
        return "DATA ERROR"
